4914c631b1f4a09af2d1dd1de8cbaded99906dab
alex-abanin <aa@occam.fi>
2024-03-29 11:20:54 +0000

    envs

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,13 @@ import { App } from "app/app";
 const container = document.getElementById("app");
 const root = createRoot(container!);
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+window.api = {
+  VITE_API_ENDPOINT: import.meta.env.VITE_API_ENDPOINT,
+  VITE_GRAPHQL_SERVER: import.meta.env.VITE_GRAPHQL_SERVER,
+};
+
 root.render(
   <StrictMode>
     <App />
