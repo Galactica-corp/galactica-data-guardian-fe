51243ec3b9bf9668f2e4c77ed8ddebbcae0b1b06
alex-abanin <aa@occam.fi>
2024-04-11 14:44:27 +0300

    new text

--- src/pages/home/ui/hero/step-metamask.tsx
+++ src/pages/home/ui/hero/step-metamask.tsx
@@ -4,8 +4,15 @@ import { StepContent } from "./step-content";
 
 export const StepMetamask = () => {
   return (
-    <StepContent title="Get your GatewayX SBT">
-      <ConnectWalletButton className="w-[266px] py-2" theme="jaffa" />
+    <StepContent
+      contentClassName="flex-col items-center mt-2"
+      title="Get your GatewayX SBT"
+    >
+      <p className="text-center text-sm font-medium text-oxfordBlue">
+        Enter in the world of Galactica & earn your unique GatewayX SBT. Create
+        your Galactica Identity <br /> and start accumulating reputation points.
+      </p>
+      <ConnectWalletButton className="mt-4 w-[266px] py-2" theme="jaffa" />
     </StepContent>
   );
 };
--- src/pages/home/ui/hero/step-x.tsx
+++ src/pages/home/ui/hero/step-x.tsx
@@ -5,10 +5,17 @@ import { StepContent } from "./step-content";
 
 export const StepX = () => {
   return (
-    <StepContent title="Get your GatewayX SBT">
+    <StepContent
+      contentClassName="flex-col items-center mt-2"
+      title="Get your GatewayX SBT"
+    >
+      <p className="text-center text-sm font-medium text-oxfordBlue">
+        Enter in the world of Galactica & earn your unique GatewayX SBT. Create
+        your Galactica Identity <br /> and start accumulating reputation points.
+      </p>
       <Button
         as={"a"}
-        className="w-[266px] whitespace-nowrap text-sm"
+        className="mt-4 w-[266px] whitespace-nowrap text-sm"
         href={`${import.meta.env.VITE_AUTH_URL}/auth`}
         referrerPolicy="no-referrer"
         theme="oxfordBlue"
