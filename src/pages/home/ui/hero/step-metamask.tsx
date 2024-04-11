3285ca56ff4a1650081cd55f007aaff589085207
alex-abanin <aa@occam.fi>
2024-04-11 14:49:36 +0300

    removed text from metamask step

--- src/pages/home/ui/hero/step-metamask.tsx
+++ src/pages/home/ui/hero/step-metamask.tsx
@@ -4,15 +4,8 @@ import { StepContent } from "./step-content";
 
 export const StepMetamask = () => {
   return (
-    <StepContent
-      contentClassName="flex-col items-center mt-2"
-      title="Get your GatewayX SBT"
-    >
-      <p className="text-center text-sm font-medium text-oxfordBlue">
-        Enter in the world of Galactica & earn your unique GatewayX SBT. Create
-        your Galactica Identity <br /> and start accumulating reputation points.
-      </p>
-      <ConnectWalletButton className="mt-4 w-[266px] py-2" theme="jaffa" />
+    <StepContent title="Get your GatewayX SBT">
+      <ConnectWalletButton className="w-[266px] py-2" theme="jaffa" />
     </StepContent>
   );
 };
